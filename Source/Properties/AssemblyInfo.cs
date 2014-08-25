f739353bfa86ff82df54b9bf71563b0f6009bcdb
Kenneth Skovhede <kenneth@hexad.dk>
2014-08-25 11:11:04 +0200

    Initial import from the SharpAESCrypt site

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpAESCrypt")]
+[assembly: AssemblyDescription("SharpAESCrypt")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HexaD")]
+[assembly: AssemblyProduct("SharpAESCrypt")]
+[assembly: AssemblyCopyright("LGPL 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90fdbfaa-1f8c-414a-afc8-c32fb99a9738")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
