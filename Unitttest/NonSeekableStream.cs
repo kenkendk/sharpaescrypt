448df3b7c1a67eb03d47939d99167ff1d9902478
FootStark <der-lueck@ist-willig.de>
2016-03-02 08:47:27 +0100

    Added Unittests: Multithread and malformed data

--- Unitttest/NonSeekableStream.cs
+++ Unitttest/NonSeekableStream.cs
@@ -14,21 +14,21 @@ namespace Unitttest
 		#region implemented abstract members of Stream
 
 		public override void Flush() { m_source.Flush(); }
-		public override long Seek(long offset, SeekOrigin origin) { throw new InvalidOperationException("Seeking not supported"); }
-		public override void SetLength(long value) { throw new InvalidOperationException("SetLength not supported"); }
+		public override long Seek(long offset, SeekOrigin origin) { throw new NotSupportedException("Seeking not supported"); }
+        public override void SetLength(long value) { throw new NotSupportedException("SetLength not supported"); }
 		public override int Read(byte[] buffer, int offset, int count) { return m_source.Read(buffer, offset, count); }
 		public override void Write(byte[] buffer, int offset, int count) { m_source.Write(buffer, offset, count); }
 		public override bool CanRead { get { return m_source.CanRead; } }
 		public override bool CanSeek { get { return false; } }
 		public override bool CanWrite { get { return m_source.CanWrite; } }
 		public override long Length 
-		{ 
-			get { throw new InvalidOperationException("Length not supported"); } 
+		{
+			get { throw new NotSupportedException("Length not supported"); } 
 		}
 		public override long Position 
-		{ 
-			get { throw new InvalidOperationException("Position not supported"); } 
-			set { throw new InvalidOperationException("Position not supported"); } 
+		{
+			get { throw new NotSupportedException("Position not supported"); }
+			set { throw new NotSupportedException("Position not supported"); } 
 		}
 
 		#endregion
