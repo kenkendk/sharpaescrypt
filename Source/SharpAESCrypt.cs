1d46ca403513a22bf6c88df09d5cfaca4c4200f0
Kenneth Skovhede <kenneth@hexad.dk>
2018-04-11 13:35:38 +0200

    Also switched out `HMAC.Create(...)`

--- Source/SharpAESCrypt.cs
+++ Source/SharpAESCrypt.cs
@@ -770,10 +770,13 @@ namespace SharpAESCrypt
                 m_crypt.BlockSize = BLOCK_SIZE * 8;
                 m_crypt.KeySize = KEY_SIZE * 8;
 
+				// TODO: Change back once we upgrade beyond netcore 2.0
+				//m_hash = HashAlgorithm.Create(HASH_ALGORITHM);
+				//m_hmac = HMAC.Create(HMAC_ALGORITHM);
+
 				m_hash = (HashAlgorithm)CryptoConfig.CreateFromName(HASH_ALGORITHM);
-                //m_hash = HashAlgorithm.Create(HASH_ALGORITHM);
                 m_rand = RandomNumberGenerator.Create(/*RAND_ALGORITHM*/);
-                m_hmac = HMAC.Create(HMAC_ALGORITHM);
+				m_hmac = (HMAC)CryptoConfig.CreateFromName(HMAC_ALGORITHM);
 
                 if (mode == OperationMode.Encrypt)
                 {
